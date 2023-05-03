b03547fa228b707a0a6b1ee1079e814c78c63de1
halder <sascha.halder91@gmail.com>
2023-05-03 13:08:16 +0200

    Implement merge sort w/ test cases

---
+++ merge_sort.py
@@ -0,0 +1,69 @@
+def merge_sort(input, asc=True):
+    """
+    Merge sort algorithm.
+    Sort list of integers in ascending (asc) or descending order
+    in n*log(n) time complexity.
+    """
+    left, right = split(input)
+    l, r = len(left), len(right)
+    
+    # base case
+    if (l == r == 1) or (min(l, r) == 0):
+        return merge(left, right, asc=asc)
+    
+    else:
+        left_tree = merge_sort(left, asc=asc)
+        right_tree = merge_sort(right, asc=asc)
+
+        return merge(left_tree, right_tree, asc=asc)
+
+def split(input):
+    h = len(input) // 2
+
+    return input[:h], input[h:]
+
+def merge(left, right, asc=True):
+    """
+    Merge sort core routine.
+    """
+    # base case
+    if not left:
+        return right
+    elif not right:
+        return left
+    
+    else:
+        l, r = len(left), len(right)
+        merged = []
+        i, j = 0, 0
+        
+        for _ in range(l + r):
+            if asc:
+                condition = (left[i] < right[j])
+            else:
+                condition = (left[i] > right[j])
+
+            if condition:
+                merged.append(left[i])
+                i += 1
+            else:
+                merged.append(right[j])
+                j += 1
+
+            # if we reach the end of either `left` or `right`
+            # we can just dump the remainder of the other list into `merged`
+            # since all elements left have to be in ascending order
+            if i == l:
+                merged.extend(right[j:])
+                break
+            if j == r:
+                merged.extend(left[i:])
+                break
+
+        return merged
+
+# test
+print(merge_sort([5,2,6,1,7,3,9]))
+print(merge_sort([3,3,1,9,4,7,12,5,2]))
+print(merge_sort([3,3,1,9,4,7,12,5,2], asc=False))
+print(merge_sort([3,3,1,2,2,2,0,2,2,2,2]))
